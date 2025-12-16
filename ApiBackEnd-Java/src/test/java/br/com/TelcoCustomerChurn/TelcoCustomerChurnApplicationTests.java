da8396a3f187c8eda9b72452196b8accfc337471
Augusto Brandão <gutoo.brandao@gmail.com>
2025-12-15 21:39:51 -0300

    Configuração Inicial

---
+++ ApiBackEnd-Java/src/test/java/br/com/TelcoCustomerChurn/TelcoCustomerChurnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.TelcoCustomerChurn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoCustomerChurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
