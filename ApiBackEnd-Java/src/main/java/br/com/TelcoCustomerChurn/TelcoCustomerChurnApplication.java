3f7ea0cf3793e5692099a6d3e57c4eb300819ed7
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-12-22 13:46:50 -0300

    fix: Ajusta configuração para rodar projeto sem DB

--- ApiBackEnd-Java/src/main/java/br/com/TelcoCustomerChurn/TelcoCustomerChurnApplication.java
+++ ApiBackEnd-Java/src/main/java/br/com/TelcoCustomerChurn/TelcoCustomerChurnApplication.java
@@ -2,8 +2,9 @@ package br.com.TelcoCustomerChurn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TelcoCustomerChurnApplication {
 
 	public static void main(String[] args) {
