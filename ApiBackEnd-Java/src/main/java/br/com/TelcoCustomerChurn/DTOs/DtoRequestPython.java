12f83988bfbeabf08f98ae920637c57d2d6b0b40
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-12-22 13:45:53 -0300

    refactor: Adiciona o uso de ENUM's em DtoRequestPython.

--- ApiBackEnd-Java/src/main/java/br/com/TelcoCustomerChurn/DTOs/DtoRequestPython.java
+++ ApiBackEnd-Java/src/main/java/br/com/TelcoCustomerChurn/DTOs/DtoRequestPython.java
@@ -1,11 +1,53 @@
 package br.com.TelcoCustomerChurn.DTOs;
 
+import br.com.TelcoCustomerChurn.Model.*;
+
+import java.math.BigDecimal;
+
+/**
+ * Sobre algumas escolhas:
+ * Nome das variaveis =
+ * Booleanos = escolhido visando a praticidade de uso na API
+ * BigDecimal = escolha padrão para lidar com valores monetários.
+ * Comentei algumas variaveis que não vi no contrato
+ *
+ * todo: Suponho que vamos precisar alterar esses campos que representam booleanos
+ * , para verdadeiros booleanos. Visando uma api escalavel
+ *
+ *         Boolean idoso,
+ *         Boolean parceiro,
+ *         Boolean dependentes,
+ *         Boolean servicoTelefone
+ *         Boolean cobrancaDigital
+ *
+ * Trazer booleanos, implica em criar um méthodo em service que converta de
+ * true/false para 1/0.
+ */
+
+
 public record DtoRequestPython(
-        Integer tempoDeContrato,
-        Integer atrasoDePagamento,
-        double usoMensal,
-        String plano,
-        double mensalidade,
-        String tipoDeContrato,
-        String metodoPagamento) {
+        Genero genero,
+        Integer idoso,
+        Integer parceiro,
+        Integer dependentes,
+        Integer tempoContratoMeses,
+        Integer servicoTelefone,
+        LinhasMultiplas linhasMultiplas,
+        TipoInternet tipoInternet,
+        SegurancaOnline segurancaOnline,
+        BackupOnline backupOnline,
+        ProtecaoDispositivo protecaoDispositivo,
+        SuporteTecnico suporteTecnico,
+        StreamingTv streamingTv,
+        StreamingFilmes streamingFilmes,
+        TipoContrato tipoContrato,
+        Integer cobrancaDigital,
+        MetodoPagamento metodoPagamento,
+        BigDecimal cobrancaMensal,
+        BigDecimal cobrancaTotal
+//        Integer atrasoDePagamento, Não vi isso na tabela de campos
+//        double usoMensal,
+//        String plano,
+//        double mensalidade,
+        ) {
 }
