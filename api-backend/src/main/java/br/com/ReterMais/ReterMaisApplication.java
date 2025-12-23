457406b0c830b71a0fb7b9c179ebe1d21259ce41
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-12-23 10:52:43 -0300

    build: ignora dependencia de JPA por hora.

--- api-backend/src/main/java/br/com/ReterMais/ReterMaisApplication.java
+++ api-backend/src/main/java/br/com/ReterMais/ReterMaisApplication.java
@@ -2,8 +2,9 @@ package br.com.ReterMais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration .class} )
 public class ReterMaisApplication {
 
 	public static void main(String[] args) {
