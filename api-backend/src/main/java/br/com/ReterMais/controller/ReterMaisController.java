cbb809211f354b0ec9d5654b235b34e8bfdfef2d
Augusto Brandão <gutoo.brandao@gmail.com>
2025-12-29 20:22:49 -0300

    fix: ajusta o dto de entrada do método e ajusta identação

--- api-backend/src/main/java/br/com/ReterMais/controller/ReterMaisController.java
+++ api-backend/src/main/java/br/com/ReterMais/controller/ReterMaisController.java
@@ -1,9 +1,9 @@
 package br.com.ReterMais.controller;
 
-import br.com.ReterMais.dtos.RequestPythonDTO;
-import br.com.ReterMais.dtos.ResponsePythonDTO;
+import br.com.ReterMais.dtos.RequestDTO;
 import br.com.ReterMais.dtos.ResponsePythonDTO;
 import br.com.ReterMais.service.ReterMaisService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,18 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reter")
 public class ReterMaisController {
 
-        private final ReterMaisService reterMaisService;
+    private final ReterMaisService reterMaisService;
 
-        public ReterMaisController (ReterMaisService reterMaisService){
-            this.reterMaisService = reterMaisService;
-        }
+    public ReterMaisController(ReterMaisService reterMaisService) {
+        this.reterMaisService = reterMaisService;
+    }
 
-        //requisicao atraves do envio do dados, para o modelo, atraves do service
-        @PostMapping("/prever")
-        public ResponseEntity<ResponsePythonDTO> prever(@RequestBody RequestPythonDTO requestPython){
-            ResponsePythonDTO response = reterMaisService.preverCancelamento(requestPython);
-            return ResponseEntity.ok(response);
-        }
-
-
+    @PostMapping("/prever")
+    public ResponseEntity<ResponsePythonDTO> prever(@Valid @RequestBody RequestDTO request) {
+        ResponsePythonDTO response = reterMaisService.preverCancelamento(request);
+        return ResponseEntity.ok(response);
+    }
 }
