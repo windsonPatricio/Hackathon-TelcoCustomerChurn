ce637356bd78a5fcc2082515e36342b3f12691d0
Augusto Brandão <gutoo.brandao@gmail.com>
2025-12-30 08:00:30 -0300

    feat: adiciona o DTO de entrada na API

--- api-backend/src/main/java/br/com/ReterMais/dtos/RequestDTO.java
+++ api-backend/src/main/java/br/com/ReterMais/dtos/RequestDTO.java
@@ -1,4 +1,28 @@
 package br.com.ReterMais.dtos;
 
-public record RequestDTO() {
+import br.com.ReterMais.model.*;
+
+import java.math.BigDecimal;
+
+public record RequestDTO(
+        String idCliente,
+        Genero genero,
+        Integer idoso,
+        Integer parceiro,
+        Integer dependentes,
+        Integer tempoContratoMeses,
+        Integer servicoTelefone,
+        LinhasMultiplas linhasMultiplas,
+        TipoInternet tipoInternet,
+        SegurancaOnline segurancaOnline,
+        BackupOnline backupOnline,
+        ProtecaoDispositivo protecaoDispositivo,
+        SuporteTecnico suporteTecnico,
+        StreamingTv streamingTv,
+        StreamingFilmes streamingFilmes,
+        TipoContrato tipoContrato,
+        Integer cobrancaDigital,
+        MetodoPagamento metodoPagamento,
+        BigDecimal cobrancaMensal,
+        BigDecimal cobrancaTotal) {
 }
