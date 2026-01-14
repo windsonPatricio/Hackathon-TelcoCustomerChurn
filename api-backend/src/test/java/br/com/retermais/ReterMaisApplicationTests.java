86f481a295e874cfba6bb4ee98704c5f83badbcd
Augusto Brandão <gutoo.brandao@gmail.com>
2026-01-14 11:14:25 -0300

    test: implementa testes unitários

--- api-backend/src/test/java/br/com/retermais/ReterMaisApplicationTests.java
+++ api-backend/src/test/java/br/com/retermais/ReterMaisApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.retermais;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ReterMaisApplicationTests {
 
     @Test
+    @DisplayName("Deve carregar o contexto da aplicação sem erros de configuração")
     void contextLoads() {
+        // Se este método rodar, significa que todos os beans (Services, Controllers, Clients)
+        // foram injetados corretamente e as propriedades foram resolvidas.
     }
 
 }
